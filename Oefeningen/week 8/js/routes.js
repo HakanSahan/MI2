0ae42feb6387c6cd415da14e7417dd10d648afe9
HakanSahan <32307236+HakanSahan@users.noreply.github.com>
2018-04-26 15:53:54 +0200

    week 8 oef 19

---
+++ Oefeningen/week 8/js/routes.js
@@ -0,0 +1,34 @@
+var routes = [
+  // Index page
+  {
+    path: '/',
+    url: './index.html',
+    name: 'home',
+  },
+  // About page
+  {
+    path: '/oef-instructies/',
+    url: './pages/oef-instructies.html',
+    name: 'instructies',
+  },
+    {
+       path: '/oef-intro',
+        url:'./pages/oef-intro.html',
+        name: 'intro',
+    },
+    {
+        path: '/oef-spel' ,
+        url:'./pages/oef-spel.html',
+        name:'spel',
+    },
+    {
+        path:'/oef-HighScore',
+        url:'./pages/oef-HighScore.html',
+        name:'HighScore',
+    },
+    {
+        path:'/oef-credits',
+        url:'./pages/oef-credits.html',
+        name:'credits' ,
+    },
+  
