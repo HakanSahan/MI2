165a9b63795b19ae7831c46cd412a230330fe25c
HakanSahan <32307236+HakanSahan@users.noreply.github.com>
2018-04-29 21:06:36 +0200

    update Project
    Ajax toegevoegd aan project

--- Project/js/Javascript.js
+++ Project/js/Javascript.js
@@ -1,64 +1,46 @@
 "use strict";
-/** class GekochtCrypto {
-    naam;
-    aantal;
-    constructor(naam, aantal, aankooprpijs){
-        this.aantal = aantal
-        this.naam = naam;
-    }
-}
-
-class cyrpto {
-    naam;
-    prijs;
-    constructor(naam,prijs){
-        this.naam = naam;
-        this.prijs = prijs;
-    }
-}
- **/
 var wallet = 100000;
-var BTC = {price: 10, name: "BTC"};
-var ABC = {price: 100, name: "ABC"};
-var CBA = {price: 5, name: "CBA"};
-var BAC = {price: 150, name: "BAC"};
-var CYC = {price: 200, name: "CYC"};
+var request = new XMLHttpRequest();
+request.open('GET','https://api.coinmarketcap.com/v1/ticker/?convert=EUR&limit=50');
+request.onload = function () {
+    console.log("You did it");
+    var crypto = JSON.parse(request.responseText);
+    for(var i =0; i <= 49; i++){
+        cryptoLijst.push(crypto[i]);
+    }
+    console.log("You did it again");
+};
+request.send();
 
 var cryptoLijst = [];
-cryptoLijst.push(BTC);
-cryptoLijst.push(ABC);
-cryptoLijst.push(CBA);
-cryptoLijst.push(BAC);
-cryptoLijst.push(CYC);
 var gekochtCryptoLijst = [];
 
 function KoopCyrpto(aantal , cryptoKopen)
 {
-    var amount = cryptoKopen.price * aantal;
+    var amount = cryptoKopen.price_eur * aantal;
     if (wallet >= amount) {
-        var index = FindIndex(gekochtCryptoLijst, cryptoKopen.name);
+        var index = FindIndex(gekochtCryptoLijst, cryptoKopen.id);
         if (index !== -1) {
             gekochtCryptoLijst[index].aantal += aantal;
         }
         else {
-            gekochtCryptoLijst.push({aantal: aantal, name: cryptoKopen.name});
+            gekochtCryptoLijst.push({aantal: aantal, id: cryptoKopen.id});
         }
-        wallet -= cryptoKopen.price * aantal;
+        wallet -= cryptoKopen.price_eur * aantal;
     }
     else {
         console.log("Not enough funds");
     }
 }
 function VerkoopCrypto(aantal, cryptoVerkopen){
-    var index = FindIndex(gekochtCryptoLijst, cryptoVerkopen.name);
+    var index = FindIndex(gekochtCryptoLijst, cryptoVerkopen.id);
     if (index !== -1) {
         if (gekochtCryptoLijst[index].aantal >= aantal){
-            wallet += cryptoVerkopen.price * aantal;
+            var index2 = FindIndex(cryptoLijst, cryptoVerkopen.id);
+            wallet += cryptoLijst[index2].price_eur * aantal;
             gekochtCryptoLijst[index].aantal = (gekochtCryptoLijst[index].aantal - aantal);
             if(gekochtCryptoLijst[index].aantal < 1){
                 gekochtCryptoLijst.splice(index,1);
-                console.log("test1");
-
             }
         }
         else {
@@ -74,15 +56,16 @@ function VerkoopCrypto(aantal, cryptoVerkopen){
 function ShowGekochtCrypto() {
     var i;
     for  (i = 0; i < gekochtCryptoLijst.length;i++){
-    console.log(gekochtCryptoLijst[i].name + " " +  gekochtCryptoLijst[i].aantal);
+    console.log(gekochtCryptoLijst[i].id + " " +  gekochtCryptoLijst[i].aantal);
     }
 }
 function FindIndex(array, name){
     var i;
-    for  (i = 0; i < gekochtCryptoLijst.length;i++){
-        if (array[i].name === name){
+    for  (i = 0; i < array.length;i++){
+        if (array[i].id === name){
             return i;
         }
     }
     return -1;
 }
+$("div").html("HEY");
